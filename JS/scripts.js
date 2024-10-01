d8b390290a0698ee76336337f158d5bd2a4fe7cb
kayytory <thelexa103@gmail.com>
2024-10-01 23:07:00 +0500

    Для другого разрешения и видео трейлера
    Трейлер не открывается во всплывающем окне, пытался разобраться, не вышло

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,7 @@
 const schemeSVG = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total")
+const menuButton = document.querySelector(".m-menu")
+const menu = document.querySelector(".menu")
 let cost = 800;
 let totalPrice = 0;
 schemeSVG.addEventListener("click", (event) => 
@@ -11,4 +13,7 @@ schemeSVG.addEventListener("click", (event) =>
 
         }
         
-    })
+});
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+})
